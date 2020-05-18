25286195657a8a74273bbf6d7d11b48942768cdf
Adi solomon <adi20782@gmail.com>
2020-05-18 21:22:09 +0300

    file completed

--- ex6.erl
+++ ex6.erl
@@ -2,18 +2,23 @@
 -author("adisolo").
 
 %% API
--export([songList/1]).
+-export([songList/1, songGen/3]).
 
 
 songList([]) -> [];
 songList(Songs) -> G = digraph:new(),
-  lists:foreach(fun(X) ->
+  lists:foreach(fun(X) -> digraph:add_vertex(G,X) end, Songs),
   lists:foreach(fun(X) -> lists:foreach(fun([Head1|Tail])->
-                                            case lists:X of
-                                              [Head1|_] -> digraph:add_edge(G, X, [Head1|Tail])
+                                            case lists:last(X) of
+                                              Head1 -> digraph:add_edge(G, X, [Head1|Tail]);
+                                              _ -> nothing
                                             end
+                                        end, Songs)
+                end, Songs), G.
 
-
-                                        end, Songs)end, Songs), G.
-
-
+songGen(G, Start, End) -> VerS = digraph:vertex(G,Start), VerE = digraph:vertex(G,End),
+                          if
+                            VerS==false; VerE==false-> error_in_graph;
+                            true -> digraph:get_short_path(G, Start, End)
+                          end.
+%%G = ex6:songList(["ABC","CBA","BAC","ACB","BBA"]).
