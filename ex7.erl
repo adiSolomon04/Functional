0a05b91e9966dc79325b236496775d38544e92ad
Adi solomon <adi20782@gmail.com>
2020-05-30 14:59:57 +0300

    tiny edit

--- ex7.erl
+++ ex7.erl
@@ -10,4 +10,28 @@
 -author("adisolo").
 
 %% API
--export([]).
+-export([steady/1, calc/3]).
+
+steady(F) ->
+{_, IoDevice} = file:open("myLog.elog", [append]),
+  StartTime = erlang:timestamp(),
+
+try F() of
+  Result -> file:write(IoDevice, to_bytes({calc_time(StartTime), success, Result}))
+catch
+  error:Error -> file:write(IoDevice,to_bytes({calc_time(StartTime), error, Error}));
+  exit:Exit -> file:write(IoDevice,to_bytes({calc_time(StartTime),exit, Exit}));
+  Throw -> file:write(IoDevice,to_bytes({calc_time(StartTime), throw, Throw}))
+end.
+
+calc_time(StartTime) ->timer:now_diff(erlang:timestamp(),StartTime).
+
+to_bytes(Output) -> io_lib:format("~p", [Output]).
+
+calc(division, A,B) when is_number(A), is_number(B) ->
+  try A/B of
+    Result -> Result
+  catch
+    error:_ -> error_divided_by_zero
+  end;
+calc(_,_,_)-> input_error.
